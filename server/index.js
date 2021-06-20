f9d07f079c0b577ab9024940ca392c9b55a57b70
Logesh <lsubramani@tekion.com>
2021-06-20 18:57:07 +0530

    changed key

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ express: 'welcome logesh ;-)' });
+  res.send({ heroku: 'welcome logesh :-)' });
 });
 
 const PORT = process.env.PORT || 5000;
